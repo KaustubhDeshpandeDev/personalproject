b99389f0004107a6ba8f8dc15bddc2b94a9f8f0b
Kaustubh Deshpande <kaustubhdeshpande@Kaustubhs-MacBook-Pro.local>
2018-08-09 16:52:04 -0500

    pushing to production

--- client/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://kaustubh:kaustubh1@ds153851.mlab.com:53851/personalproject',
-  secretOrKey: 'secret'
-};
--- client/server.js
+++ client/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -10,14 +11,14 @@ const posts = require("./routes/api/posts");
 const app = express();
 
 // Body parser middleware
-console.log(`${__dirname}/../build`);
-app.use(express.static(`${__dirname}/../build`));
+
+app.use(express.static(`${__dirname}/build`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
-
+const db = process.env.CONNECTION_STRING;
+console.log(process.env.CONNECTION_STRING);
 // Connect to MongoDB
 mongoose
   .connect(db)
@@ -35,7 +36,7 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-const port = 4005;
+const port = process.env.PORT || 4005;
 
 const path = require("path");
 app.get("*", (req, res) => {
