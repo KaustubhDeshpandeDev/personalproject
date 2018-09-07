8563811c726be232b4231ef6a6611391fc665919
Kaustubh Deshpande <kaustubhdeshpande@Kaustubhs-MacBook-Pro.local>
2018-09-07 16:44:28 -0500

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -79,13 +79,6 @@ class App extends Component {
                   component={EditProfile}
                 />
               </Switch>
-              {/* <Switch>
-                <PrivateRoute
-                  exact
-                  path="/add-experience"
-                  component={AddExperience}
-                />
-              </Switch> */}
               <Switch>
                 <PrivateRoute
                   exact
